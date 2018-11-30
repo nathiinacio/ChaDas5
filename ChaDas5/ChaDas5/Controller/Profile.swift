f51b76407a02c327abe6db933dc7b7bdcbef487e
Gabriela Szpilman <gabi_szpilman@hotmail.com>
2018-11-30 19:15:37 -0200

    segmented control profile

--- ChaDas5/ChaDas5/Controller/Profile.swift
+++ ChaDas5/ChaDas5/Controller/Profile.swift
@@ -8,6 +8,67 @@
 
 import UIKit
 
+
+
+
+
+//struct MyStory {
+//    var postDate:Date
+//    var title:String
+//    var description:String
+//}
+//
+//var myStories:[MyStory] = []
+//
+//  var datasources = [myStories]
+
+
+
 class Profile: UIViewController {
     
+    
+    var segmentedControl: CustomSegmentedContrl!
+
+    @IBOutlet weak var profileLabel: UILabel!
+    
+    
+    
+    override func viewDidLoad() {
+        segmentedControl = CustomSegmentedContrl.init(frame: CGRect.init(x: 0, y: 300, width: self.view.frame.width, height: 45))
+        segmentedControl.backgroundColor = .white
+        segmentedControl.commaSeperatedButtonTitles = "first, two"
+        segmentedControl.addTarget(self, action: #selector(onChangeOfSegment(_:)), for: .valueChanged)
+        
+        self.view.addSubview(segmentedControl)
+        
+    }
+    
+    
+    
+    
+    @objc func onChangeOfSegment(_ sender: CustomSegmentedContrl) {
+        
+        
+        profileLabel.text = "meu segmento é: \(sender.selectedSegmentIndex)"
+    }
+    
+//    @IBOutlet weak var profileSegmentedControl: UISegmentedControl!
+//
+//    @IBAction func segmentedTapped(_ sender: Any) {
+//
+//        let getIndex = profileSegmentedControl.selectedSegmentIndex
+//
+//        switch (getIndex) {
+//        case 0:
+//            self.profileLabel.backgroundColor = UIColor.red
+//        case 1:
+//            self.profileLabel.backgroundColor = UIColor.blue
+//        default:
+//            self.profileLabel.backgroundColor = UIColor.red
+//        }
+    
 }
+    
+
+    
+
