fd86f1919bd6971a712e3bcc0cdbd462f250ebbb
juliarocha <juliaafr@outlook.com>
2018-12-16 13:57:49 -0200

    detalhes finais do merge

---
+++ ChaDas5/ChaDas5/Model/New Group/Firebase.swift
@@ -0,0 +1,14 @@
+//
+//  Firebase.swift
+//  ChaDas5
+//
+//  Created by Julia Rocha on 10/12/18.
+//  Copyright © 2018 Julia Maria Santos. All rights reserved.
+//
+
+import Firebase
+
+
+class FBRef {
+    static var db:Firestore!
+}
