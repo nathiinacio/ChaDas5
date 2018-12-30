50732866707408ef188cf7d47207795a5b5403ed
Nathalia Inacio <nathaliainacio@NathaliaInacio.local>
2018-12-30 14:28:46 -0200

    login pronto

--- ChaDas5/ChaDas5/Controller/CreateNewAccount.swift
+++ ChaDas5/ChaDas5/Controller/CreateNewAccount.swift
@@ -14,6 +14,7 @@ import Firebase
 class CreateNewAccount: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource {
     
     var selected:ChooseYourTeaCollectionViewCell?
+    var index: IndexPath?
     
     
     //outlets
@@ -22,12 +23,51 @@ class CreateNewAccount: UIViewController, UICollectionViewDelegate, UICollection
     @IBOutlet weak var passwordConfirmationTextField: UITextField!
     @IBOutlet weak var pickYourTeaCollectionView: UICollectionView!    
     @IBOutlet weak var createNewAccountButton: UIButton!
+
     
-    //var yourTea: String!
+    private let db = Firestore.firestore()
+    
     var activityView:UIActivityIndicatorView!
+    
     //actions
     @IBAction func createNewButton(_ sender: Any) {
+        
+        if passwordTextField.text == passwordConfirmationTextField.text && selected?.chooseYourTeaLabel.text != nil
+        {
+            handleSignUp()
+            //ir para o feed checar no save profile
+        }
+        else{
+            
+            if passwordTextField.text != passwordConfirmationTextField.text {
+                
+            let tentarNovamente = UIAlertAction(title: "Tentar Novamente", style: .default, handler: { (action) -> Void in
+                self.passwordConfirmationTextField.text = ""
+                self.passwordTextField.text = ""
+            })
+            let alert = UIAlertController(title: "Oops...", message: "Erro na Confirmação de Senha", preferredStyle: .alert)
+            alert.addAction(tentarNovamente)
+            self.present(alert, animated: true, completion: nil)
+            alert.view.tintColor = UIColor.buttonPink
+                
+            }
+            
+            else {
+                let ok = UIAlertAction(title: "Ok", style: .default, handler: { (action) -> Void in
+                })
+                let alert = UIAlertController(title: "Oops...", message: "Esqueceu de Escolher seu Chá", preferredStyle: .alert)
+                alert.addAction(ok)
+                self.present(alert, animated: true, completion: nil)
+                alert.view.tintColor = UIColor.buttonPink
+                
+            }
+        }
+        
+        
+        
+        
     }
+    
     @IBAction func dismissButton(_ sender: Any) {
         dismiss()
     }
@@ -35,6 +75,8 @@ class CreateNewAccount: UIViewController, UICollectionViewDelegate, UICollection
     override func viewDidLoad() {
         hideKeyboardWhenTappedAround()
         
+        passwordTextField.isSecureTextEntry = true
+        passwordConfirmationTextField.isSecureTextEntry = true
         
         //collection view settings
         pickYourTeaCollectionView.allowsMultipleSelection = false
@@ -45,11 +87,18 @@ class CreateNewAccount: UIViewController, UICollectionViewDelegate, UICollection
         let nib = UINib.init(nibName: "ChooseYourTeaCollectionViewCell", bundle: nil)
         self.pickYourTeaCollectionView.register(nib, forCellWithReuseIdentifier: "PickYouTea")
         
+        activityView = UIActivityIndicatorView(style: .gray)
+        activityView.color = UIColor.buttonPink
+        activityView.frame = CGRect(x: 0, y: 0, width: 50.0, height: 50.0)
+        activityView.center = createNewAccountButton.center
+        
+        view.addSubview(activityView)
         
         emailTextField.addTarget(self, action: #selector(textFieldChanged), for: .editingChanged)
         passwordTextField.addTarget(self, action: #selector(textFieldChanged), for: .editingChanged)
         passwordConfirmationTextField.addTarget(self, action: #selector(textFieldChanged), for: .editingChanged)
-        
+    
+        setcreateNewAccountButton(enabled: false)
         
         
     }
@@ -57,7 +106,6 @@ class CreateNewAccount: UIViewController, UICollectionViewDelegate, UICollection
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         emailTextField.becomeFirstResponder()
-        //        NotificationCenter.default.addObserver(self, selector:#selector(keyboardWillAppear), name: NSNotification.Name.UIResponder.keyboardWillShowNotification, object: nil)
         
     }
     
@@ -107,6 +155,7 @@ class CreateNewAccount: UIViewController, UICollectionViewDelegate, UICollection
         let selectedCell = collectionView.cellForItem(at: indexPath) as! ChooseYourTeaCollectionViewCell
         selectedCell.contentView.backgroundColor = UIColor.basePink
         self.selected = selectedCell
+        self.index = collectionView.indexPath(for: selected!)
         print("foi")
     
     }
@@ -120,33 +169,34 @@ class CreateNewAccount: UIViewController, UICollectionViewDelegate, UICollection
         let email = emailTextField.text
         let passwordConfirmed = passwordConfirmationTextField.text
         let password = passwordTextField.text
-        let formFilled = email != nil && email != "" && passwordConfirmed != nil && passwordConfirmed != "" && password != nil && password != ""
+        
+        let formFilled = email != nil && email != "" && passwordConfirmed != nil && passwordConfirmed != "" && password != nil && password != "" 
         setcreateNewAccountButton(enabled: formFilled)
     }
     
     
     
-//    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
-//
-//        // Resigns the target textField and assigns the next textField in the form.
-//
-//        switch textField {
-//        case emailTextField:
-//            emailTextField.resignFirstResponder()
-//            passwordTextField.becomeFirstResponder()
-//            break
-//        case passwordTextField:
-//            passwordTextField.resignFirstResponder()
-//            passwordConfirmationTextField.becomeFirstResponder()
-//            break
-//        case passwordConfirmationTextField:
-//            handleSignUp()
-//            break
-//        default:
-//            break
-//        }
-//        return true
-//    }
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        
+        // Resigns the target textField and assigns the next textField in the form.
+        
+        switch textField {
+        case emailTextField:
+            emailTextField.resignFirstResponder()
+            passwordTextField.becomeFirstResponder()
+            break
+        case passwordTextField:
+            passwordTextField.resignFirstResponder()
+            passwordConfirmationTextField.becomeFirstResponder()
+            break
+        case passwordConfirmationTextField:
+            handleSignUp()
+            break
+        default:
+            break
+        }
+        return true
+    }
     
     
     @objc func handleSignUp() {
@@ -154,66 +204,128 @@ class CreateNewAccount: UIViewController, UICollectionViewDelegate, UICollection
         guard let pass = passwordTextField.text else { return }
         guard let yourTea = self.selected!.chooseYourTeaLabel.text else { return }
         
+
         setcreateNewAccountButton(enabled: false)
         createNewAccountButton.setTitle("", for: .normal)
         activityView.startAnimating()
         
-        Auth.auth().createUser(withEmail: email, password: pass) { user, error in
-            if error == nil && user != nil {
-                print("User created!")
-                
-        let changeRequest = Auth.auth().currentUser?.createProfileChangeRequest()
-        changeRequest?.displayName = email
-               
-                changeRequest?.commitChanges { error in
-                    if error == nil {
-                        print("User display name changed!")
-                        
-                        self.saveProfile(username: yourTea) { success in
-                            if success {
-                                self.dismiss(animated: true, completion: nil)
-                            } else {
-                                self.resetForm()
+        
+        
+        Auth.auth().fetchProviders(forEmail: email, completion: { (stringArray, error) in
+            if error != nil {
+                print(error!)
+            } else {
+                if stringArray == nil {
+                    print("No password. No active account")
+                    Auth.auth().createUser(withEmail: email, password: pass) { user, error in
+                        if error == nil && user != nil {
+                            print("User created!")
+                            
+                            let changeRequest = Auth.auth().currentUser?.createProfileChangeRequest()
+                            changeRequest?.displayName = yourTea
+                            
+                            changeRequest?.commitChanges { error in
+                                if error == nil {
+                                    print("User display name changed!")
+                                    
+                                    self.saveProfile(username: yourTea) { success in
+                                        if success {
+                                            self.dismiss(animated: true, completion: nil)
+                                        } else {
+                                            self.resetForm()
+                                        }
+                                    }
+                                    
+                                } else {
+                                    print("Error: \(error!.localizedDescription)")
+                                    self.resetForm()
+                                }
+                            }
+                            
+                        } else {
+                            self.resetForm()
                         }
                     }
-       
+                    
+                } else {
+                    print("There is an active account")
+                    let tentarNovamente = UIAlertAction(title: "Tentar Novamente", style: .default, handler: { (action) -> Void in
+                        self.emailTextField.text = ""
+                        self.passwordConfirmationTextField.text = ""
+                        self.passwordTextField.text = ""
+                        //self.pickYourTeaCollectionView.deselectAllItems(animated: true)
+                        
+                    })
+        
+                    let cancelar = UIAlertAction(title: "Cancelar", style: .default ) { (action) -> Void in
+                        self.dismiss()
+                    }
+        
+                    let alert = UIAlertController(title: "Oops...", message: "E-mail já usado anteriormente", preferredStyle: .alert)
+                    alert.addAction(tentarNovamente)
+                    alert.addAction(cancelar)
+                    self.present(alert, animated: true, completion: nil)
+                    alert.view.tintColor = UIColor.buttonPink
+        
+                    self.setcreateNewAccountButton(enabled: true)
+                    self.createNewAccountButton.setTitle("Criar Conta", for: .normal)
+                    self.activityView.stopAnimating()
                 }
             }
-        }
+        })
+        
+        
     }
-
     
-}
+
+    func saveProfile(username:String, completion: @escaping ((_ success:Bool)->())) {
+        guard let uid = Auth.auth().currentUser?.uid else { return }
+        
+        db.collection("users").document("\(uid)").setData([
+            "username": username
+        ]) { err in
+            if let err = err {
+                print("Error writing document: \(err)")
+            } else {
+                print("Document successfully written!")
+                //ir para feed
+            }
+        }
+        
+    }
+    
+    func resetForm() {
+       
+        let tentarNovamente = UIAlertAction(title: "Tentar Novamente", style: .default, handler: { (action) -> Void in
+            self.emailTextField.text = ""
+            self.passwordConfirmationTextField.text = ""
+            self.passwordTextField.text = ""
+            //self.pickYourTeaCollectionView.deselectAllItems()
+        })
+        
+        let cancelar = UIAlertAction(title: "Cancelar", style: .default ) { (action) -> Void in
+            self.dismiss()
+        }
+        
+        let alert = UIAlertController(title: "Oops...", message: "Ocorreu algum erro com seus dados", preferredStyle: .alert)
+        alert.addAction(tentarNovamente)
+        alert.addAction(cancelar)
+        self.present(alert, animated: true, completion: nil)
+        alert.view.tintColor = UIColor.buttonPink
+        
+        setcreateNewAccountButton(enabled: true)
+        createNewAccountButton.setTitle("Criar Conta", for: .normal)
+        activityView.stopAnimating()
+    }
+    
+    
+
     
     @objc private func dismiss() {
         self.dismiss(animated: true, completion: nil)
         
     }
-    
-    func resetForm() {
-        let alert = UIAlertController(title: "Error signing up", message: nil, preferredStyle: .alert)
-        alert.addAction(UIAlertAction(title: "Okay", style: .default, handler: nil))
-        self.present(alert, animated: true, completion: nil)
-        
-        setcreateNewAccountButton(enabled: true)
-        createNewAccountButton.setTitle("Continue", for: .normal)
-        activityView.stopAnimating()
-    }
-    
-    
-    func saveProfile(username:String, completion: @escaping ((_ success:Bool)->())) {
-        guard let uid = Auth.auth().currentUser?.uid else { return }
-        let databaseRef = Database.database().reference().child("users/profile/\(uid)")
-        
-        let userObject = [
-            "username": username,
-            ] as [String:Any]
-        
-        databaseRef.setValue(userObject) { error, ref in
-            completion(error == nil)
-        }
-    }
-    
+
     
     /**
      Enables or Disables the **continueButton**.
@@ -230,7 +342,12 @@ class CreateNewAccount: UIViewController, UICollectionViewDelegate, UICollection
     }
     
     
-    
-    
+}
+extension UICollectionView {
+    func deselectAllItems(animated: Bool = false) {
+        for indexPath in self.indexPathsForSelectedItems ?? [] {
+            self.deselectItem(at: indexPath, animated: animated)
+        }
+    }
 }
 
--- ChaDas5/ChaDas5/Controller/Login.swift
+++ ChaDas5/ChaDas5/Controller/Login.swift
@@ -6,7 +6,9 @@
 //  Copyright © 2018 Julia Maria Santos. All rights reserved.
 //
 
+import Foundation
 import UIKit
+import Firebase
 
 class Login: UIViewController {
     
@@ -15,20 +17,156 @@ class Login: UIViewController {
     @IBOutlet weak var passwordTextField: UITextField!
     @IBOutlet weak var loginButton: UIButton!
     
+    var activityView:UIActivityIndicatorView!
+    
     //action
     @IBAction func dismissButton(_ sender: Any) {
         dismiss()
     }    
     @IBAction func loginButton(_ sender: Any) {
+        handleSignIn()
+        print("BORA PARA A PROXIMA PAGINA")
     }
     
     override func viewDidLoad() {
         hideKeyboardWhenTappedAround()
+        passwordTextField.isSecureTextEntry = true
+        
+        activityView = UIActivityIndicatorView(style: .gray)
+        activityView.color = UIColor.buttonPink
+        activityView.frame = CGRect(x: 0, y: 0, width: 50.0, height: 50.0)
+        activityView.center = loginButton.center
+        
+        view.addSubview(activityView)
+        
+        emailTextField.delegate = self as? UITextFieldDelegate
+        passwordTextField.delegate = self as? UITextFieldDelegate
+        emailTextField.addTarget(self, action: #selector(textFieldChanged), for: .editingChanged)
+        passwordTextField.addTarget(self, action: #selector(textFieldChanged), for: .editingChanged)
+
+        setLoginButton(enabled: false)
+        
+    }
+    
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        emailTextField.becomeFirstResponder()
+        //NotificationCenter.default.addObserver(self, selector:#selector(keyboardWillAppear), name: NSNotification.Name.UIKeyboardWillShow, object: nil)
+        
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        emailTextField.resignFirstResponder()
+        passwordTextField.resignFirstResponder()
+        NotificationCenter.default.removeObserver(self)
+    }
+    
+    override var preferredStatusBarStyle: UIStatusBarStyle {
+        get {
+            return .lightContent
+        }
+    }
+    
+    @objc func keyboardWillAppear(notification: NSNotification){
+        
+        let info = notification.userInfo!
+        let keyboardFrame: CGRect = (info[UIResponder.keyboardFrameEndUserInfoKey] as! NSValue).cgRectValue
+        
+        loginButton.center = CGPoint(x: view.center.x,
+                                        y: view.frame.height - keyboardFrame.height - 16.0 - loginButton.frame.height / 2)
+        activityView.center = loginButton.center
+    }
+    
+    @objc func textFieldChanged(_ target:UITextField) {
+        let email = emailTextField.text
+        let password = passwordTextField.text
+        let formFilled = email != nil && email != "" && password != nil && password != ""
+        setLoginButton(enabled: formFilled)
+    }
+    
+    
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        
+        // Resigns the target textField and assigns the next textField in the form.
+        
+        switch textField {
+        case emailTextField:
+            emailTextField.resignFirstResponder()
+            passwordTextField.becomeFirstResponder()
+            break
+        case passwordTextField:
+            handleSignIn()
+            break
+        default:
+            break
+        }
+        return true
+    }
+    
+    /**
+     Enables or Disables the **continueButton**.
+     */
+    
+    
+    @objc func handleSignIn() {
+        guard let email = emailTextField.text else { return }
+        guard let pass = passwordTextField.text else { return }
+        
+        setLoginButton(enabled: false)
+        loginButton.setTitle("", for: .normal)
+        activityView.startAnimating()
+        
+        Auth.auth().signIn(withEmail: email, password: pass) { user, error in
+            if error == nil && user != nil {
+                //self.dismiss(animated: false, completion: nil)
+                print ("Logado com sucesso!")
+            } else {
+                print("Error logging in: \(error!.localizedDescription)")
+                
+                self.resetForm()
+            }
+        }
+    }
+    
+    func resetForm() {
+        
+        let tentarNovamente = UIAlertAction(title: "Tentar Novamente", style: .default, handler: { (action) -> Void in
+            self.emailTextField.text = ""
+            self.passwordTextField.text = ""
+        })
+        
+        let cancelar = UIAlertAction(title: "Cancelar", style: .default ) { (action) -> Void in
+            self.dismiss()
+        }
+        
+        let alert = UIAlertController(title: "Erro ao Logar", message: nil, preferredStyle: .alert)
+        alert.addAction(tentarNovamente)
+        alert.addAction(cancelar)
+        self.present(alert, animated: true, completion: nil)
+        alert.view.tintColor = UIColor.buttonPink
+        
+        setLoginButton(enabled: true)
+        loginButton.setTitle("Fazer Login", for: .normal)
+        activityView.stopAnimating()
     }
     
     @objc private func dismiss() {
         self.dismiss(animated: true, completion: nil)
         
+    }
+    
+    
+    func setLoginButton(enabled:Bool) {
+        if enabled {
+            loginButton.alpha = 1.0
+            loginButton.isEnabled = true
+        } else {
+            loginButton.alpha = 0.5
+            loginButton.isEnabled = false
+        }
     }
     
 }
